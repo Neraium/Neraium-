72df3e044a9076c812e5dcf6cccbae19b76e17eb
motocraig-ux <motocraig@icloud.com>
2026-03-04 18:45:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const reveals = document.querySelectorAll(".reveal")
+const reveals=document.querySelectorAll(".reveal")
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll",()=>{
 
 for(let i=0;i<reveals.length;i++){
 
-let windowHeight = window.innerHeight
-let revealTop = reveals[i].getBoundingClientRect().top
-let revealPoint = 120
+let windowHeight=window.innerHeight
+let elementTop=reveals[i].getBoundingClientRect().top
+let revealPoint=120
 
-if(revealTop < windowHeight - revealPoint){
+if(elementTop<windowHeight-revealPoint){
 reveals[i].classList.add("active")
 }
 
